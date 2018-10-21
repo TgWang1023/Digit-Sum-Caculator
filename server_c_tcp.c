213f230347dbe3b4cf15d73f0b0a1da313f83a64
Tiange Wang <wtgkevin@gmail.com>
2018-10-21 15:41:01 -0700

    checking if string is num

--- server_c_tcp.c
+++ server_c_tcp.c
@@ -1,6 +1,84 @@
 /* 
-    Tiange Wang 
-    ID: 3717659
-    CS 176A
-    Homework 2
+    Tiange Wang, ID: 3717659, CS 176A Homework 2
+    Code Cited: http://www.linuxhowtos.org/data/6/server.c, Sockets Tutorial, Example TCP Server.
 */
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     char response[256] = "From Server: ";
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"usage: %s port\n", argv[0]);
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("%s", buffer);
+     int digitOnly = 1;
+     if(strlen(buffer) == 0) {
+         digitOnly = 0;
+         n = write(newsockfd,"From Server: Sorry, cannot compute!",35);
+         if (n < 0) error("ERROR writing to socket");
+     }
+     for(int i = 0; i < strlen(buffer); i++) {
+         if(!isdigit((unsigned char)buffer[i])) {
+             digitOnly = 0;
+             n = write(newsockfd,"From Server: Sorry, cannot compute!",35);
+             if (n < 0) error("ERROR writing to socket");
+             break;
+         }
+     }
+     if(digitOnly == 1) {
+         while(strlen(buffer) > 1) {
+            int total = 0;
+            for(int i = 0; i < strlen(buffer); i++) {
+                total += (buffer[i] - '0');
+            }
+            sprintf(buffer, "%i", total);
+            strcat(response, buffer);
+            n = write(newsockfd, response, strlen(response));
+            if (n < 0) error("ERROR writing to socket");
+            strcpy(response, "From Server: ");
+         }
+     }
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
--- server_c_udp.c
+++ server_c_udp.c
@@ -1,6 +1,4 @@
 /* 
-    Tiange Wang 
-    ID: 3717659
-    CS 176A
-    Homework 2
+    Tiange Wang, ID: 3717659, CS 176A Homework 2
+    Code Cited: geeksforgeeks.org, Example C client side program. Author: Akshat Sinha
 */
